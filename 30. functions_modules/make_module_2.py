2cd99abf6130e103d85700d82684b310087b5df4
Renato Bianco <renato.bianco@hotmail.com>
2021-10-06 17:18:30 +0100

    created folders for multiple files

--- 30. functions_modules/make_module_2.py
+++
@@ -1,6 +0,0 @@
-def make_panuozzo(size, *toppings):
-    print(f"Making an amazing {size}-cm panuozzo with the following toppings:")
-    
-    for topping in toppings:
-        print(f"- {topping.title()}")
-
